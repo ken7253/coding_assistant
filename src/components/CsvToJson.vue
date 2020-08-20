cd65e6113a67d763fdc44ce67fd7c2f462afab1f
ken7253 <ken7253@gmail.com>
2020-08-20 12:33:56 +0900

    create CsvToJson components.

---
+++ src/components/CsvToJson.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="csv-to-json">
+      <label for="">CSV file<input type="file"></label>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            csvFile: "",
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
