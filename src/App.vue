0883db81ed3bce693e7361beff1cf6952d495359
ken7253 <ken7253@gmail.com>
2020-08-24 01:10:09 +0900

    add html elements

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
-      <AppHeader/>
-      <RadixConversion/>
-      <ColorConversion/>
+    <AppHeader />
+    <RadixConversion />
+    <ColorConversion />
+    <CreateJsonFile />
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader'
-import RadixConversion from './components/RadixConversion'
-import ColorConversion from './components/ColorConversion'
+import AppHeader from "./components/AppHeader";
+import RadixConversion from "./components/RadixConversion";
+import ColorConversion from "./components/ColorConversion";
+import CreateJsonFile from "./components/CreateJsonFile";
 
 export default {
-    name: 'app',
-    components: {
-        AppHeader,
-        RadixConversion,
-        ColorConversion,
-    }
-}
+  name: "app",
+  components: {
+    AppHeader,
+    RadixConversion,
+    ColorConversion,
+    CreateJsonFile,
+  },
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- src/components/createJsonFIle.vue
+++ src/components/createJsonFIle.vue
@@ -1,13 +1,32 @@
 <template>
-    <div class="create-json-flie"></div>
+  <div class="create-json-flie">
+    <h2>JSON生成</h2>
+    <table>
+      <tr>
+        <th><input type="text" placeholder="Key" /></th>
+        <td><input type="text" placeholder="Value" /></td>
+        <td>
+            <select>
+                <option value="text">text</option>
+                <option value="number">number</option>
+                <option value="object">object</option>
+            </select>
+        </td>
+        <td>
+            <input type="button" value="+" @click="addNewRow" />
+            <input type="button" value="-" @click="removeThisRow"/>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function() {
+    return {};
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
