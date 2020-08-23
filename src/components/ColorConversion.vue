6910957ac4eb0d9385058dfbc7577002c07cdba8
ken7253 <ken7253@gmail.com>
2020-08-23 19:38:43 +0900

    add func checkNumLength

--- src/components/ColorConversion.vue
+++ src/components/ColorConversion.vue
@@ -2,33 +2,53 @@
   <div class="color-conversion">
     <h2>カラーコード変換</h2>
     <input v-model="colorCode" type="text" />
-    <input @click="moldNum" type="button" value="変換">
+    <input @click="moldNum" type="button" value="変換" />
     <ul>
       <li>red:{{ rgbColor[0] }}</li>
       <li>green:{{ rgbColor[1] }}</li>
       <li>blue:{{ rgbColor[2] }}</li>
     </ul>
     <p>for CSS</p>
+    <p>{{ checkNumFormat("#fff") }}</p>
     <code>rgb({{ rgbColor[0] }},{{ rgbColor[1] }},{{ rgbColor[2] }})</code>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      colorCode: '',
-      rgbColor:'0',
+      colorCode: "000000",
+      rgbColor: [0, 0, 0],
     };
   },
   methods: {
     moldNum: function() {
-      const moldArr = this.colorCode.match(/.{2}/g);
-      const outArr = [];
-      moldArr.forEach((element) => {
-        outArr.push(this.hexConv(element));
+      const checkedNum = this.checkNumLength(this.colorCode);
+      const splitArr = checkedNum.match(/.{2}/g);
+      const outputArr = [];
+      splitArr.forEach((element) => {
+        outputArr.push(this.hexConv(element));
       });
-      this.rgbColor = outArr;
+      this.rgbColor = outputArr;
+    },
+    checkNumLength: function(num) {
+      const numLength = num.length;
+      if (numLength == 6) {
+        return num;
+      } else if (numLength == 3) {
+        const arr = num.split("");
+        const outputArr = [];
+        arr.forEach((element) => {
+          outputArr.push(element + element);
+        });
+        return outputArr.join("");
+      } else {
+        console.error(
+          "error: An incorrect value has been entered as a color code."
+        );
+        return "000000";
+      }
     },
     hexConv: function(num) {
       const hexNum = "0x" + num;
