bdbc13e6bf79414a328276ff5473abdfb966dd61
ken7253 <ken7253@gmail.com>
2020-08-25 00:33:40 +0900

    Splitting Components ’CreateJsonFile’

---
+++ src/components/InputTableRow.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="input-table-row">
+    <tr>
+      <td>
+        <input v-model.trim="content.jsonKey" type="text" placeholder="Key" />
+      </td>
+      <td>
+        <input
+          v-model.trim="content.jsonValue"
+          type="text"
+          placeholder="Value"
+        />
+      </td>
+      <td>
+        <select v-model="content.valueType">
+          <option disabled value="">select type</option>
+          <option>text</option>
+          <option>number</option>
+          <option>object</option>
+        </select>
+      </td>
+      <td>
+        <!--
+            <input type="button" value="+" @click="addNewRow" />
+            <input type="button" value="-" @click="removeThisRow"/>
+          -->
+      </td>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {
+      data: function() {
+    return {
+      content: {
+        jsonKey: "",
+        jsonValue: null,
+        valueType: "",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/createJsonFIle.vue
+++ src/components/createJsonFIle.vue
@@ -2,29 +2,18 @@
   <div class="create-json-flie">
     <h2>JSON生成</h2>
     <table>
-      <tr>
-        <th><input type="text" placeholder="Key" /></th>
-        <td><input type="text" placeholder="Value" /></td>
-        <td>
-            <select>
-                <option value="text">text</option>
-                <option value="number">number</option>
-                <option value="object">object</option>
-            </select>
-        </td>
-        <td>
-            <input type="button" value="+" @click="addNewRow" />
-            <input type="button" value="-" @click="removeThisRow"/>
-        </td>
-      </tr>
+      <InputTableRow/>
     </table>
   </div>
 </template>
 
 <script>
+import InputTableRow from "./InputTableRow";
+
 export default {
-  data: function() {
-    return {};
+  name: "CreateJsonFile",
+  components: {
+    InputTableRow,
   },
 };
 </script>
