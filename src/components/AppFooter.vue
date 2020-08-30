79c0e1c40587c9c29eb790c805c95502a71b14ff
ken7253 <ken7253@gmail.com>
2020-08-30 19:06:30 +0900

    create footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="app-footer">
-      <p>&#169; {{ yearNow }} ken7253.</p>
+    <p>
+      &#169; -<time v-once>{{ getYearNow() }}</time> ken7253
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-    data: function(){
-        return {
-            yearNow: 0,
-        }
-    }
-}
+  data: function() {
+    return {};
+  },
+  methods: {
+    getYearNow: function() {
+      const getNowDate = new Date();
+      return getNowDate.getFullYear();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/sass/_var.scss";
 .app-footer {
-    width: 100%;
-    text-align: center;
+  position: fixed;
+  bottom: 0;
+  padding: 0.25rem 0 0.5rem 0;
+  z-index: 1000;
+  width: 100%;
+  text-align: center;
+  background-color: $text-color;
+  color: $white-color;
+  p {
+    margin: 0;
+    font-size: 0.75rem;
+  }
 }
 </style>
