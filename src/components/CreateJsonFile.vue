3e577695ce6b657ecdf4f457076e33e1cb6a7dd5
ken7253 <ken7253@gmail.com>
2020-09-03 01:11:15 +0900

    add json Download func and fix scss.

--- src/components/CreateJsonFile.vue
+++ src/components/CreateJsonFile.vue
@@ -12,17 +12,21 @@
       <input @click="removeRow" type="button" value="-" />
     </div>
     <table>
+      <tr>
+        <td></td>
+        <td>KEY</td>
+        <td>VALUE</td>
+      </tr>
       <tr v-for="(line, index) in lines" v-bind:key="index">
         <th>{{ index }}</th>
         <td><input v-model="line.key" type="text" placeholder="KEY" /></td>
         <td><input v-model="line.value" type="text" placeholder="VALUE" /></td>
-        <td></td>
       </tr>
     </table>
 
     <div>
       <code>{{ previewData }}</code>
-      <input @click="createPreview" type="button" value="Preview">
+      <input @click="createPreview" type="button" value="Preview" />
       <input @click="downloadJson" type="button" value="Download" />
     </div>
   </div>
@@ -56,16 +60,28 @@ export default {
     downloadJson: function() {
       this.createPreview();
       // ダウンロード処理
+      const data = this.previewData;
+      console.log(this.previewData);
+      const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+      const blob = new Blob([bom, data], { type: "text/json" });
+      const url = (window.URL || window.webkitURL).createObjectURL(blob);
+      const link = document.createElement("a");
+      link.download = "result.json";
+      link.href = url;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
       return console.log(this.previewData);
     },
     createPreview: function() {
-      const json = new Object;
+      // JSONに整形
+      const json = new Object();
       for (let index in this.lines) {
         const key = this.lines[index].key;
         const value = this.lines[index].value;
         json[key] = value;
       }
-      return this.previewData = json;
+      return (this.previewData = JSON.stringify(json));
     },
   },
 };
@@ -73,6 +89,9 @@ export default {
 
 <style lang="scss" scoped>
 @import "../assets/sass/_var.scss";
+input,select {
+  margin-right: 1rem;
+}
 input[type="text"] {
   margin-right: 1rem;
 }
