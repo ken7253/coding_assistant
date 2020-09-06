f5a6333f8021b2baaa709d91801c60950f53c6b3
ken7253 <ken7253@gmail.com>
2020-09-06 09:35:00 +0900

    add site-link footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="app-footer">
-    <p>
+    <p class="copyright">
       &#169; -<time v-once>{{ getYearNow() }}</time> ken7253
     </p>
+    <div class="link-pages">
+      <ul class="flex-wrp-ard">
+        <li v-for="(siteData, index) in siteDatas" :key="index">
+          <a :href="siteData[0]" :taget="isBlank(siteData[2])" rel="noopener">{{
+            siteData[1]
+          }}</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data: function() {
-    return {};
+    return {
+      siteDatas: {
+        // 0:url 1:display name 2: (target="_blank") flag
+        author: ["https://dairoku-studio.com/", "Author", true],
+        github: ["https://github.com/ken7253/coding_assistant", "Github", true],
+      },
+    };
   },
   methods: {
     getYearNow: function() {
       const getNowDate = new Date();
       return getNowDate.getFullYear();
+    },
+    isBlank: function(flag = false) {
+      return flag ? "_blank" : "_self";
     },
   },
 };
@@ -23,10 +41,7 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/sass/_var.scss";
 .app-footer {
-  position: fixed;
-  bottom: 0;
   padding: 0.25rem 0 0.5rem 0;
-  z-index: 1000;
   width: 100%;
   text-align: center;
   background-color: $color-text;
@@ -34,6 +49,17 @@ export default {
   p {
     margin: 0;
     font-size: 0.75rem;
+  }
+}
+.link-pages {
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    li a {
+      color: $color-theme;
+      font-weight: 500;
+    }
   }
 }
 </style>
